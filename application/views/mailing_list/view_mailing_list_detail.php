f4760d3a02235a585afeb9cbbdd34ce2dd16f1b9
Nick Nedev <nedevn@gmail.com>
2014-05-11 22:07:40 -0700

    Fixes the detail view.

--- application/views/mailing_list/view_mailing_list_detail.php
+++ application/views/mailing_list/view_mailing_list_detail.php
@@ -3,14 +3,23 @@
 if ($query->num_rows>0) : ?>
 
 <?php foreach ($query->result() as $row) : ?>
-	<h1>  </h1>
-	<p>
-<p>	User Id:<?php $row->userid; ?></p>
-		<?php $row->first_name;?>
-		<?php $row->last_name; ?>
+	<h3> <?=$row->first_name;?> <?=$row->last_name;?> </h3>
+	
+	<p>	User Id: <?=$row->userid;?></p>
+	<p>	First Name: <?=$row->first_name;?></p>
+	<p>	Last Name: <?=$row->last_name;?></p>
+	<p>	Email: <?=$row->email;?></p>
+	<p>	Address: <?=$row->address;?></p>
+	<p>	Zip Code: <?=$row->zip_postal;?></p>
+	<p>	Username: <?=$row->username;?></p>
+	<p>	Bio: <?=$row->bio;?></p>
+
+	<p>	Tours: <?=$row->num_tours;?></p>
+
+
 		
 	
-</p>
+	</p>
 <?php endforeach; ?>
 
 <?php endif; ?>  
